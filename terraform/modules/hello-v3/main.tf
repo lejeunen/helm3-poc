25d583b7ad8e25c0cec8aa06bc290e905bf063a4
Nicolas Lejeune <lejeune.n@gmail.com>
2020-07-15 11:07:30 +0200

    Terragrunt and Terraform configuration for hello app in v3

---
+++ terraform/modules/hello-v3/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "local" {}
+
+  # The latest version of Terragrunt (v0.19.0 and above) requires Terraform 0.12.0 or above.
+  required_version = ">= 0.12.0"
+}
+
+provider "helm" {
+  version = "~> 1.2.3"
+}
+
+# helm3-poc repository must have been defined
+resource "helm_release" "hello_v3" {
+  name = "hello-v3"
+  chart = "helm3-poc/hello-v2"
+  namespace = var.app_namespace
+  version = var.chart_version
+}
